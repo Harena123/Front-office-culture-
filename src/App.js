fa1818a93807340e082031dbd61b23350ad41596
Andrianina24 <rakotozananya5@gmail.com>
2024-02-04 19:50:53 +0300

    links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-// import logo from './logo.svg';
-import Login from './components/Login';
-// import Culture from './components/Culture';
-// import Validation from './components/Validation';
-// import Dashboard from './components/Dashboard';
-import Liste from './components/Liste';
-import Simulation from './components/Simulation';
+import Liste from "./components/Liste";
+import Culture from "./components/Culture";
+import Dashboard from "./components/Dashboard";
+import Items from "./components/Items";
+import Login from "./components/Login";
+import Page from "./components/Page";
+import Simulation from "./components/Simulation";
+import Validation from "./components/Validation";
 
-
-import './App.css';
-import './Dash.css';
-
+import "./App.css";
+import "./Dash.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <Simulation></Simulation>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} exact />
+        <Route path="/culture" element={<Culture />} exact />
+        <Route path="/dashboard" element={<Dashboard />} exact />
+        <Route path="/items" element={<Items />} exact />
+        <Route path="/liste" element={<Liste />} exact />
+        <Route path="/page" element={<Page />} exact />
+        <Route path="/simulation" element={<Simulation />} exact />
+        <Route path="/validation" element={<Validation />} exact />
+      </Routes>
+    </Router>
   );
 }
 export default App;
